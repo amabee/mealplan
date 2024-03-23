56904072eb3a26d68fb34facd89324a28a10fa28
amabee <cyberkiller.ck@gmail.com>
2024-03-23 10:41:17 +0800

    refactor: Fixed Null Error on sched meals

--- lib/scheduled_meal.dart
+++ lib/scheduled_meal.dart
@@ -42,21 +42,106 @@ class _ScheduledMealPageState extends State<ScheduledMealPage> {
         body: FutureBuilder<List<dynamic>>(
           future: _plannedMealsFuture,
           builder: (context, snapshot) {
+            print(snapshot);
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             } else if (snapshot.hasError) {
               return Center(child: Text('Error: ${snapshot.error}'));
             } else {
+              if (snapshot.data == null || snapshot.data!.isEmpty) {
+                return Center(child: Text('Nothing to show here'));
+              }
               List<dynamic> plannedMeals = snapshot.data!;
-              return ListView.builder(
-                itemCount: plannedMeals.length,
-                itemBuilder: (context, index) {
-                  return _buildMealBox(plannedMeals[index]);
-                },
+              Map<String, List<dynamic>> groupedMeals =
+                  groupMealsByDate(plannedMeals);
+              return ListView(
+                children: groupedMeals.entries.map((entry) {
+                  return _buildDateCard(entry.key, entry.value);
+                }).toList(),
               );
             }
           },
         ),
+      ),
+    );
+  }
+
+  Map<String, List<dynamic>> groupMealsByDate(List<dynamic> meals) {
+  Map<String, List<dynamic>> groupedMeals = {};
+  for (var meal in meals) {
+    String? startDate = meal['start_date'];
+    if (startDate != null) {
+      String endDate = meal['end_date'];
+      String formattedStartDate = _formatDate(startDate);
+      if (!groupedMeals.containsKey(formattedStartDate)) {
+        groupedMeals[formattedStartDate] = [];
+      }
+      groupedMeals[formattedStartDate]!.add(meal);
+    }
+  }
+  return groupedMeals;
+}
+
+
+  String _formatDate(String? date) {
+    if (date != null) {
+      DateTime dateTime = DateTime.parse(date);
+      return '${_getMonthName(dateTime.month)} ${dateTime.day}';
+    } else {
+      return 'Unknown';
+    }
+  }
+
+  String _getMonthName(int month) {
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return '';
+    }
+  }
+
+  Widget _buildDateCard(String? date, List<dynamic> meals) {
+    return Card(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '$date',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Column(
+            children: meals.map((meal) {
+              return _buildMealBox(meal);
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
@@ -66,30 +151,26 @@ class _ScheduledMealPageState extends State<ScheduledMealPage> {
       onTap: () {
         // Handle meal selection
       },
-      child: Container(
-        margin: EdgeInsets.all(10.0),
-        padding: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              meal['title'],
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Start Date: ${meal['start_date']}',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            Text(
-              'End Date: ${meal['end_date']}',
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ],
+      child: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          padding: EdgeInsets.all(15.0),
+          width: 200,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  '${meal['title']}',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -122,11 +203,11 @@ class _ScheduledMealPageState extends State<ScheduledMealPage> {
         }
       } else {
         print(response.statusCode);
-        return [];
+        return [""];
       }
     } catch (error) {
       print(error);
-      return [];
+      return [""];
     }
   }
 }
